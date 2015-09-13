ef9c08a770509ca1fc873a89dd5223b5b86dfacb
Alvaro Garcia <derecerca@gmail.com>
2015-09-13 23:32:32 +0200

    save WIP signatures

--- Spec.hs
+++ Spec.hs
@@ -1,11 +1,9 @@
 main =putStrLn "hello"
 
-toBinary :: Integer -> [Integer]
 toBinary 0 = [0]
 toBinary n = reverse $ toBinary' [] maxExponent n
     where maxExponent = floor (logBase 2 n)
 
-toBinary'  :: (Ord a, Num a1, Num a, Integral b, Eq a1) => [a1] -> b -> a -> [a1]
 toBinary' xs (-1) _ = xs
 toBinary' xs exponent n = toBinary' (current : xs) (exponent - 1) (rest)
     where currentNumber = 2^exponent
